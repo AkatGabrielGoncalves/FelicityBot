59240b1a0d3eaa5c2c5287554e9cc8877004cac8
AkatGabrielGoncalves <gabrielmatheus9a@gmail.com>
2021-09-01 19:17:39 -0300

    remove mongoose remains

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import Discord from 'discord.js';
 import * as dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import intents from './intents';
 import { commandsHandler } from './modules/commandsHandler';
 
 // This will load our BOT_TOKEN from the .env file so we can access at process.env
 dotenv.config();
-
-const mongoString = process.env.MONGO_CONNECTION_STRING || '';
 
 /* intents are all the events that this bot will listen,
  which is everything. You can change this behavior at ./intents.ts */
@@ -17,7 +14,6 @@ const client = new Discord.Client({ intents });
 client.on('messageCreate', (message) => commandsHandler(client, message));
 
 // Well, this creates a connection to our bot
-mongoose.connect(mongoString).then(() => console.log('Connected to DB'));
 client
   .login(process.env.BOT_TOKEN)
   .then(() => console.log('Bot is online'))
