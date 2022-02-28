32883637c8d5f025d7ef91e34c4f4b18553fd598
AkatGabrielGoncalves <gabrielmatheus9a@gmail.com>
2022-02-28 12:47:02 -0300

    chore(MusicPlayer):Remove testing console logs

--- src/modules/music/MusicPlayer.ts
+++ src/modules/music/MusicPlayer.ts
@@ -90,7 +90,6 @@ export class MusicPlayer extends PlayerQueue {
    * Stop or play the next song. */
   private playerDecisionMaker = async () => {
     try {
-      console.log('RUNNING PLAYER DECISION MAKER');
       const playerIsReady = () => this.conn?.state.status === 'ready';
 
       if (this.channel.members.size === 1) return await this.internalStop(this.message);
@@ -146,10 +145,6 @@ export class MusicPlayer extends PlayerQueue {
       stream.on('error', (err) => {
         stream.kill('SIGTERM');
         Logger.log('ERROR', 'Spawn failed!', err);
-      });
-
-      stream.stderr!.on('data', (data) => {
-        console.log(Buffer.from(data).toString());
       });
 
       stream.unref();
