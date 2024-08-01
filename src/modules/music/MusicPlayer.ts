f64455f9ae28ccbaec8213f2e7cb34444d565557
Gabriel <66503407+AkatGabrielGoncalves@users.noreply.github.com>
2024-08-01 00:29:48 -0300

    Fix args of ffmpeg for yt-dlp

--- src/modules/music/MusicPlayer.ts
+++ src/modules/music/MusicPlayer.ts
@@ -163,7 +163,7 @@ export class MusicPlayer extends PlayerQueue {
           '--external-downloader',
           'ffmpeg',
           '--external-downloader-args',
-          '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 10',
+          '-reconnect:1 -reconnect_streamed:1 -reconnect_delay_max:10',
           url,
         ],
         { stdio: [0, 'pipe', 'pipe'] }
